073789113e5a5e7019a9d88e9807c34e9f7b5e88
Ivan Semkin <ivan@semkin.ru>
2018-10-16 03:22:53 +0300

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="umling",
+    version="0.0.1",
+    author="Ivan Semkin",
+    author_email="ivan@semkin.ru",
+    description="",  # TODO
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vanyasem/umling",
+    license='GPLv3+',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: Russian",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+)
